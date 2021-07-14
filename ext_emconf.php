aecee00951a119a383326d0b50f7a9966a8e3ee3
Stephan Ude <ude@mediatis.de>
2021-07-14 17:13:44 +0200

    [TASK] code base, dependencies, typoscript, dispatcher and destination

---
+++ ext_emconf.php
@@ -0,0 +1,26 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Form Relay - JSON Request Plugin',
+    'description' => 'Send form data via JSON HTTP requests',
+    'category' => 'be',
+    'author' => '',
+    'author_email' => '',
+    'author_company' => 'Mediatis AG',
+    'state' => 'alpha',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '9.5.0-10.4.99',
+            'formrelay' => '>=2.0.0'
+        ],
+        'conflicts' => [
+        ],
+    ],
+    'suggests' => [
+    ],
+];
